6d65b91dff0c8c2f1e17691c834888708128aa2b
zhangchao <13722436010@163.com>
2025-03-13 20:30:52 +0800

    小程序商品浏览业务代码开发

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -43,15 +43,15 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
         }
 
         //1、从请求头中获取令牌
-        String token = request.getHeader(jwtProperties.getUserTokenName());
+        String token = request.getHeader(jwtProperties.getAdminTokenName());
 
         //2、校验令牌
         try {
             log.info("jwt校验:{}", token);
-            Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
-            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
-            log.info("当前用户id：{}", userId);
-            BaseContext.setCurrentId(userId);
+            Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
+            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
+            log.info("当前员工id：{}", empId);
+            BaseContext.setCurrentId(empId);
             //3、通过，放行
             return true;
         } catch (Exception ex) {
